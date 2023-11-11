2d8b35a0bed33a0830121026af079e9abc20a9b8
VukadinovicLuka <lvukadinovic2921rn@raf.rs>
2023-11-11 13:04:07 +0100

    FileExporter

---
+++ Projekat/Specifikacija/src/main/java/api/SpecFileExport.java
@@ -0,0 +1,10 @@
+package api;
+
+import java.io.FileNotFoundException;
+
+public abstract class SpecFileExport {
+
+    public abstract void exportFile(String path) throws FileNotFoundException;
+
+
+}
